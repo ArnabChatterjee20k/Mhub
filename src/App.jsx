88ce8ae270ed5e1aad1feb96110f58031f7e77b6
arnabchatterjee <arnabchatterjee.ac.2@gmail.com>
2022-09-27 20:01:34 +0530

    basic setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Container from '@mui/material/Container';
+import CssBaseline  from '@mui/material/CssBaseline';
+import  Typography  from '@mui/material/Typography';
+function App() {
+  return (
+    <>
+      <CssBaseline />
+      <Container fixed>
+        <Typography>Hellow</Typography>
+      </Container>
+    </>
+  )
+}
+
+export default App
